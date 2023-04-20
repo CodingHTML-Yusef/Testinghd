1a522691536ea01297b798f01b0fdc74517a73b8
Cobalt-60 <95923271+plastics-eater@users.noreply.github.com>
2023-04-20 13:57:14 -0400

    Update theme.js

--- static/js/theme.js
+++ static/js/theme.js
@@ -4,12 +4,10 @@ const themes = [
     'dark',
     'frost'
 ];
-const snow = document.getElementsByClassName("snow");
 
 function changeTheme(theme) {
     if (themes.includes(theme)) {
         document.body.setAttribute('data-theme', theme);
-        document.snow.setAttribute('data-theme', theme);
         localStorage.setItem('theme', theme);
     } else {
         alert('That is not a theme');
